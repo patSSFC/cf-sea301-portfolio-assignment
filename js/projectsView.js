d9b5a451c37779e227e5ae6d8b194c2c72990dc2
TAYLOR WIRTZ <taylor.wirtz@gmail.com>
2016-03-07 19:47:49 -0800

    indicate which 'page' the user is on

--- js/projectsView.js
+++ js/projectsView.js
@@ -1,18 +1,28 @@
 var projectView = {};
 
-projectView.handleNav = function() {
-  $('.nav-links').on('click', function(event){
+var theme = {
+  selectedTextColor: 'white-text',
+  selectedBackground: 'purple',
+  unselectedTextColor: 'purple-text',
+  unselectedBackground: 'white',
+};
+
+projectView.selected = '';
+projectView.handleNav = function () {
+  $('.nav-links').on('click', function (event) {
     $('.tab-content').hide();
+    $('.nav-links').removeClass('selected').addClass('unselected');
+    $(this).toggleClass('selected unselected');
     $('#' + $(this).data('content')).fadeIn(500);
   });
 
   $('.nav-links:last').click();
 };
 
-projectView.setTeasers = function() {
+projectView.setTeasers = function () {
   $('.project-body *:nth-of-type(n+2)').hide();
 
-  $('#projects').on('click', '.read-on', function(e){
+  $('#projects').on('click', 'a.read-on', function (e) {
     e.preventDefault();
     if ($(this).hasClass('read-on')) {
       $(this).parentsUntil('article').find('.project-body').children().show();
@@ -21,16 +31,19 @@ projectView.setTeasers = function() {
   });
 };
 
-projectView.navHover = function() {
+projectView.navHover = function () {
   //TODO -- apply / remove selected class so the selected button applies
-  $('.nav-links').on('mouseover', function(e) {
-    $(e.target).children().addClass('white-text');
-    $(e.target).children().removeClass('purple-text');
+  $('.nav-links').on('mouseover', function (e) {
+    $evTarget = $(e.target);
+    $(e.target).children().addClass('white-text purple');
+    $(e.target).removeClass('purple-text unselected');
+
   });
 
-  $('.nav-links').on('mouseleave', function(e){
-    $(e.target).children().addClass('purple-text');
-    $(e.target).children().removeClass('white-text');
+  $('.nav-links').on('mouseleave', function (e) {
+    $evTarget = $(e.target);
+    $evTarget.children().addClass('purple-text');
+    $evTarget.children().removeClass('white-text purple');
   });
 };
 
